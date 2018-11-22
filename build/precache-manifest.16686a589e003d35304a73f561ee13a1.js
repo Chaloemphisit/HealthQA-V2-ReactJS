8b771b0baaed07bcc7ae7a368597dc75a90bbc05
Chaloemphisit Sirichai <Chaloemphisit@users.noreply.github.com>
2018-11-22 15:29:26 +0700

    build production

---
+++ build/precache-manifest.16686a589e003d35304a73f561ee13a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d73a69a92326e2498418935110f4b0",
+    "url": "/static/media/header-logo.98d73a69.png"
+  },
+  {
+    "revision": "6236b80d971bcea03825e2123b4d6c50",
+    "url": "/static/media/comment_background.6236b80d.svg"
+  },
+  {
+    "revision": "359266bc704f10424f3444cde1eb8835",
+    "url": "/static/media/Avatar-User.359266bc.svg"
+  },
+  {
+    "revision": "0af1b36b0ec5367570f2fcdbab255383",
+    "url": "/static/media/Avatar-Doctor-Men.0af1b36b.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e63ebec7c6a5ad187bbf",
+    "url": "/static/js/main.e63ebec7.chunk.js"
+  },
+  {
+    "revision": "058e97c53a6b5450ddd9",
+    "url": "/static/js/1.058e97c5.chunk.js"
+  },
+  {
+    "revision": "e63ebec7c6a5ad187bbf",
+    "url": "/static/css/main.e30a54cc.chunk.css"
+  },
+  {
+    "revision": "058e97c53a6b5450ddd9",
+    "url": "/static/css/1.33133eea.chunk.css"
+  },
+  {
+    "revision": "e5e06739c725138a44bbba3000e51a43",
+    "url": "/index.html"
+  }
+];
